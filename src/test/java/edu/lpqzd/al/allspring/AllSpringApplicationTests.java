6b6dfd8be97cd64a422bd34098d7d6f2dfe73e6f
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-07-21 07:49:41 -0500

    Inicialización del pruyecto

---
+++ src/test/java/edu/lpqzd/al/allspring/AllSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lpqzd.al.allspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
